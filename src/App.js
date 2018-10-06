fda5f67d3fe56aa47f05cd95ca59f88ad0617c5f
Meo Huynh <mayoh727@gmail.com>
2018-10-05 17:08:24 -0700

    Import FoursquareAPI into App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FoursquareAPI from './API/';
 
 class App extends Component {
+  componentDidMount() {
+    FoursquareAPI.search({
+      near: 'San Diego',
+      query: 'sushi',
+      limit: 10
+    }).then(results => console.log(results));
+  }
+
   render() {
     return (
       <div className="App">
