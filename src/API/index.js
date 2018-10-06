894f6680ef13b81b3a4461909eea961b5caaa461
Meo Huynh <mayoh727@gmail.com>
2018-10-05 17:08:24 -0700

    Add helper class with baseURL method

---
+++ src/API/index.js
@@ -0,0 +1,7 @@
+// This helper class has a baseURL function that returns the base URL that is used often
+// Won't have to type out the same URL; just need to add on to it
+class Helper {
+  static baseURL() {
+    return "http://api.foursquare.com/v2";
+  }
+}
