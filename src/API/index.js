91670990d66c1513f204932303ed4a3639cff8f9
Meo Huynh <mayoh727@gmail.com>
2018-10-05 17:08:24 -0700

    Fix version_parameter to v per Foursquare
    Foursquare requires the version parameter to be just the letter
    'v' and not anything else. Anything else will not work.

--- src/API/index.js
+++ src/API/index.js
@@ -9,7 +9,7 @@ class Helper {
     const clientKeys = {
       client_id: 'HGDYIWSS0FUEOPKOBZISUXYROHPNASRXN54EJDVE0XAQZO12',
       client_secret: 'JXZYMNLQADTJJDYQDYDAQMAOBIOFFT01P0YLHH32SWBRUFX1',
-      version_parameter: '20181005'
+      v: '20181005'
     };
     // Object.keys(keys) returns an array with each value turned into strings
     // The map() returns an array with the property names' values into strings
