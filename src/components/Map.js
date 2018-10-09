a544e23f5c1db870254f737fb5ce3f9b7ee158e4
Meo Huynh <mayoh727@gmail.com>
2018-10-08 19:09:53 -0700

    Add Google Map API component

---
+++ src/components/Map.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+// Below component code imported from https://tomchentw.github.io/react-google-maps/
+// Used to render Google API Maps
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const MyMapComponent = withScriptjs(withGoogleMap(props => (
+    <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
+      {props.isMarkerShown && (
+        <Marker position={{ lat: -34.397, lng: 150.644 }} />
+      )}
+    </GoogleMap>
+  ))
+);
+
+export default class Map extends Component {
+  render() {
+    return (
+      <MyMapComponent
+        isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBfyYKVKGUTbiaDlAPYZjRGRAnexfUeBjU"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `400px` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    );
+  }
+}
