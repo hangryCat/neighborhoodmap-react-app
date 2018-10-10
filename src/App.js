48107c01201ee7d23eade1b1dec77c5dfc01cbcf
Meo Huynh <mayoh727@gmail.com>
2018-10-10 15:48:48 -0700

    Add markerClose method to markerClick

--- src/App.js
+++ src/App.js
@@ -20,7 +20,10 @@ class App extends Component {
 // The Object.assign() method is used to copy the values of all enumerable own properties
 // from the source object(s) to a target object. It will return the target object
   // The current marker is assigned the property from the the 'this.state.markers'
+// this.markerClose() is called first to close any open markers before setting
+  // the currently clicked marker to isOpen = true
   markerClick = marker => {
+    this.markerClose();
     marker.isOpen = true;
     this.setState({ markers: Object.assign(this.state.markers, marker) });
   }
