daa7a550c5aca7204e7892213e5d898fdeb94fb4
Meo Huynh <mayoh727@gmail.com>
2018-10-11 15:46:41 -0700

    Render VenueList

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import VenueList from './VenueList';
 
 export default class SideBar extends Component {
   render() {
     return (
       <div className="sidebar">
-        
+        <VenueList />
       </div>
     );
   }
