c8b2bc7a0935aca8095cee9ed2560e45032c4281
Meo Huynh <mayoh727@gmail.com>
2018-10-11 15:46:25 -0700

    Render ListItem

--- src/components/VenueList.js
+++ src/components/VenueList.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import ListItem from './ListItem';
 
 export default class VenueList extends Component {
   render() {
     return (
       <ol className="venue-list">
-
+        <ListItem />
       </ol>
     );
   }
