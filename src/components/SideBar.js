ad8b3fb9fb50c29e52885bb3e2af0bad1bd83410
Meo Huynh <mayoh727@gmail.com>
2018-10-15 18:17:35 -0700

    Initialize local state for search queries

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,6 +2,15 @@ import React, { Component } from 'react';
 import VenueList from './VenueList';
 
 export default class SideBar extends Component {
+  // Initialize local state by assigning the query object within this.state
+  // The query will be used to handle search filters of venues
+  constructor() {
+    super();
+    this.state = {
+      query: ''
+    };
+  }
+
   render() {
     return (
       <div className="sidebar">
