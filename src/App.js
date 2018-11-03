21b95d839722f84926086135b613f127c14e8043
Meo Huynh <mayoh727@gmail.com>
2018-11-03 16:42:29 -0700

    Move h1 to App component

--- src/App.js
+++ src/App.js
@@ -87,6 +87,7 @@ class App extends Component {
     // {...this.state} adds the all of the state declared above components below
     return (
       <div className="App">
+        <header><h1>Sushi Restaurants in San Diego, CA</h1></header>
         <SideBar {...this.state} listItemClick={this.listItemClick} />
         <Map {...this.state } markerClick={this.markerClick} />
       </div>
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -45,7 +45,6 @@ export default class SideBar extends Component {
   render() {
     return (
       <div className="sidebar">
-        <h1>Sushi Restaurants in San Diego, CA</h1>
         <input type={"search"} id={"search"} placeholder={"Search"} onChange={this.searchVenuesMarkers} />
         <VenueList {...this.props} venues={this.handleVenuesList()} listItemClick={this.props.listItemClick} />
       </div>
