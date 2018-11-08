0bdee80d4900afe172733b92d8f4216d4958c217
Meo Huynh <mayoh727@gmail.com>
2018-11-07 18:01:04 -0800

    Add tabindex

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 export default class ListItem extends Component {
   render() {
     return (
-      <li className="list-item" onClick={() => this.props.listItemClick(this.props)}>
+      <li tabindex={2} className="list-item" onClick={() => this.props.listItemClick(this.props)}>
         <img src={this.props.categories[0].icon.prefix + "32" + this.props.categories[0].icon.suffix}
           alt={this.props.categories[0].name} />
-          
+
         {this.props.name}
       </li>
     );
